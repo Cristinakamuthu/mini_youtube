8ea843af1ef7b52435572db4e7962ca58ea646d1
Cristina <kamuthucristina@gmail.com>
2025-05-28 14:43:25 +0300

    written the code for the routes

---
+++ src/pages/Death.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Death = () => {
+  return (
+    <div>Death</div>
+  )
+}
+
+export default Death
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,15 +1,72 @@
-
-
-
-
+import App from "./App";
+import EasterPage from "./pages/EasterPage";
+import Errorpage from "./pages/Errorpage";
+import HolyspiritPage from "./pages/HolyspiritPage";
+import Home from "./pages/Home";
+import LentenPage from "./pages/LentenPage";
+import MarianPage from "./pages/MarianPage";
+import Mass from "./pages/Mass";
+import OccasionsPage from "./pages/OccasionsPage";
+import PraisePage from "./pages/PraisePage";
+import BaptismPage from "./pages/BaptismPage";
+import DeathPage from "./pages/DeathPage";
 
 const routes = [
-    path : "/"
-    element
-
-
- 
-    
-
-
-]
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Errorpage />,
+  },
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <Errorpage />,
+  },
+  {
+    path: "/masssongs",
+    element: <Mass />,
+    errorElement: <Errorpage />,
+  },
+  {
+    path: "/lentensongs",
+    element: <LentenPage />,
+    errorElement: <Errorpage />,
+  },
+  {
+    path: "/Eastersongs",
+    element: <EasterPage />,
+    errorElement: <Errorpage />,
+  },
+  {
+    path: "/mariansongs",
+    element: <MarianPage />,
+    errorElement: <Errorpage />,
+  },
+  {
+    path: "/HolySpiritsongs",
+    element: <HolyspiritPage />,
+    errorElement: <Errorpage />,
+  },
+  {
+    path: "/Occasions",
+    element: <OccasionsPage />,
+    errorElement: <Errorpage />,
+    children: [
+      {
+        path: "baptism",
+        element: <BaptismPage />,
+        errorElement: <Errorpage />,
+      },
+      {
+        path: "death",
+        element: <DeathPage />,
+        errorElement: <Errorpage />,
+      },
+    ],
+  },
+  {
+    path: "/Praise",
+    element: <PraisePage />,
+    errorElement: <Errorpage />,
+  },
+];
